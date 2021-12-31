b2c9b7a3658bca71dec1631f8b2bd8152b06393d
adivc21 <adi.vc21@gmail.com>
2021-12-30 19:55:33 -0500

    Committing 141. Linked List Cycle solution

---
+++ 0141_Linked_List_Cycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        checked_nodes = set()
+        
+        while head:
+            if head in checked_nodes:
+                return True
+            else:
+                checked_nodes.add(head)
+                head = head.next
+                
+        return False
