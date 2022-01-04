fd4edd8e30b3dfcc0a661c6a08a31d6c0028e97b
adivc21 <adi.vc21@gmail.com>
2022-01-04 14:42:35 -0500

    Added a couple more solutions to 217. Contains Duplicate.

--- 0217_Contains_Duplicate.py
+++ 0217_Contains_Duplicate.py
@@ -5,5 +5,35 @@ class Solution:
         is greater than the number of unique elements
         in nums
         '''
-        return len(nums) > len(set(nums))
+        # # -----x-----x-----
+        # # Solution 1 - Length of Set
+        # # Time complexity = O(n)
+        # # Space complexity = O(n)
+        # return len(nums) > len(set(nums))
+        # # -----x-----x-----
+
+        # # -----x-----x-----
+        # # Solution 2 - Set
+        # # Time complexity = O(n)
+        # # Space complexity = O(n)
+        # nums_set = set()
+        
+        # for n in nums:
+        #     if n in nums_set:
+        #         return True
+        #     nums_set.add(n)
+            
+        # return False
+        # # -----x-----x-----
+        
+        # # -----x-----x-----
+        # # Solution 3 - Sort
+        # # Time complexity = Depends on sorting algorithm used
+        # # Space complexity = O(1)
+        nums.sort()
+        
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]: return True
+        return False
+        # # -----x-----x-----
         
