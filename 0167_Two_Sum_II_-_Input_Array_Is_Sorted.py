3aeb0f2107a041eca6e3c719e9a4f4234218a248
adivc21 <adi.vc21@gmail.com>
2022-01-10 18:41:48 -0500

    Binary search solution for 167. Two Sum II - Input Array Is Sorted problem

--- 0167_Two_Sum_II_-_Input_Array_Is_Sorted.py
+++ 0167_Two_Sum_II_-_Input_Array_Is_Sorted.py
@@ -6,16 +6,36 @@ class Solution:
         # # Time complexity = O(n)
         # # Space complexity = O(1)
         
+        # left = 0
+        # right = len(numbers) - 1
+        
+        # while left < right:
+        #     if (numbers[left]+numbers[right] < target):
+        #         left += 1
+        #     elif (numbers[left]+numbers[right] > target):
+        #         right -= 1
+        #     else:
+        #         break
+                
+        # return [left+1, right+1]
+        # # -----x-----x-----
+        
+        # # -----x-----x-----
+        # # Solution 2 - Two Pointers with Binary Search
+        # # Time complexity = O(log(n))
+        # # Space complexity = O(1)
+        
         left = 0
         right = len(numbers) - 1
         
         while left < right:
-            if (numbers[left]+numbers[right] < target):
-                left += 1
-            elif (numbers[left]+numbers[right] > target):
-                right -= 1
-            else:
+            mid = left + ((right-left)//2)
+            if (numbers[left]+numbers[right] == target):
                 break
+            elif (numbers[left]+numbers[right] < target):
+                left = mid if (numbers[mid]+numbers[right] <= target) else (left+1)
+            else:
+                right = mid if (numbers[left]+numbers[mid] >= target) else (right-1)
                 
         return [left+1, right+1]
-        # # -----x-----x-----
+        # # -----x-----x-----      
