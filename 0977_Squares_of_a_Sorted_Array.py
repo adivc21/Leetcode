704648a7e8808e25ba9b648b83365128fc6e3e12
adivc21 <adi.vc21@gmail.com>
2022-01-13 15:18:58 -0500

    Solution for 977. Squares of a Sorted Array problem

---
+++ 0977_Squares_of_a_Sorted_Array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        
+        # # -----x-----x-----
+        # # Solution 1 - One pass
+        # # Time complexity = O(n)
+        # # Space complexity = O(1)
+                
+        n = right = len(nums)-1
+        left = 0
+        res = [0] * len(nums)
+        
+        while left <= right:
+            
+            if abs(nums[right]) > abs(nums[left]):
+                res[n] = nums[right] * nums[right]
+                right -= 1
+            else:
+                res[n] = nums[left] * nums[left]
+                left += 1
+                
+            n -= 1
+            
+        return res
+        # # -----x-----x-----
