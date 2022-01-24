c5bca7a8c181125460e7b41f394960bacb93fb22
adivc21 <adi.vc21@gmail.com>
2022-01-24 16:40:12 -0500

    Three solutions for the 144. Binary Tree Preorder Traversal problem

--- 0144_Binary_Tree_Preorder_Traversal.py
+++ 0144_Binary_Tree_Preorder_Traversal.py
@@ -6,21 +6,58 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if not(root):
-            return list()
+
+        # # -----x-----x-----
+        # # Solution 1 - Recursion
+        # # Time complexity = O(n)
+        # # Space complexity = O(n)
+
+        # if root:
+        #     return ([root.val]
+        #            + self.preorderTraversal(root.left)
+        #            + self.preorderTraversal(root.right))
+        # return []
+        # # -----x-----x-----
+         
+        # # -----x-----x-----
+        # # Solution 2 - Iteration, using stack
+        # # Time complexity = O(n)
+        # # Space complexity = O(2n)        
+
+        # if not(root):
+        #     return list()
         
-        node_stack = [root]
-        nodes = []
+        # node_stack = [root]
+        # nodes = []
         
-        while node_stack:
-            root = node_stack.pop()
+        # while node_stack:
+        #     root = node_stack.pop()
             
-            if root:
-                nodes.append(root.val)
+        #     if root:
+        #         nodes.append(root.val)
                 
-                if root.right:
-                    node_stack.append(root.right)
-                if root.left:
-                    node_stack.append(root.left)
+        #         if root.right:
+        #             node_stack.append(root.right)
+        #         if root.left:
+        #             node_stack.append(root.left)
                     
-        return nodes
+        # return nodes
+        # # -----x-----x-----
+
+        # # -----x-----x-----
+        # # Solution 3 - Iteration, using stack, condensed
+        # # Time complexity = O(n)
+        # # Space complexity = O(2n)    
+        node = root
+        res, stack = list(), list()
+        
+        while node or stack:
+            while node:
+                res.append(node.val)
+                stack.append(node)
+                node = node.left
+                
+            node = stack.pop().right    
+            
+        return res
+        # # -----x-----x-----        
